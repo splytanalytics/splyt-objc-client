673a6b142d9e52e759663d6c1ba7da38d701f66b
stoutfiles <shawn.stout@knetik.com>
2017-04-03 23:29:00 -0400

    JSAPI Objective C API update

---
+++ SwaggerClient/Api/DCCTransactionsApi.m
@@ -0,0 +1,379 @@
+#import "DCCTransactionsApi.h"
+#import "DCCQueryParamCollection.h"
+#import "DCCApiClient.h"
+#import "DCCDataCollectorBeginTransactionRequest.h"
+#import "DCCDataCollectorEndTransactionRequest.h"
+#import "DCCDataCollectorUpdateCollectionRequest.h"
+#import "DCCDataCollectorUpdateTransactionRequest.h"
+
+
+@interface DCCTransactionsApi ()
+
+@property (nonatomic, strong, readwrite) NSMutableDictionary *mutableDefaultHeaders;
+
+@end
+
+@implementation DCCTransactionsApi
+
+NSString* kDCCTransactionsApiErrorDomain = @"DCCTransactionsApiErrorDomain";
+NSInteger kDCCTransactionsApiMissingParamErrorCode = 234513;
+
+@synthesize apiClient = _apiClient;
+
+#pragma mark - Initialize methods
+
+- (instancetype) init {
+    return [self initWithApiClient:[DCCApiClient sharedClient]];
+}
+
+
+-(instancetype) initWithApiClient:(DCCApiClient *)apiClient {
+    self = [super init];
+    if (self) {
+        _apiClient = apiClient;
+        _mutableDefaultHeaders = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+#pragma mark -
+
+-(NSString*) defaultHeaderForKey:(NSString*)key {
+    return self.mutableDefaultHeaders[key];
+}
+
+-(void) setDefaultHeaderValue:(NSString*) value forKey:(NSString*)key {
+    [self.mutableDefaultHeaders setValue:value forKey:key];
+}
+
+-(NSDictionary *)defaultHeaders {
+    return self.mutableDefaultHeaders;
+}
+
+#pragma mark - Api Methods
+
+///
+/// Begins a new transaction
+/// Use the event properties to describe the initial state of the transaction
+///  @param customerId customerId 
+///
+///  @param request Transaction initiation information (optional)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) beginTransactionUsingPOSTWithCustomerId: (NSString*) customerId
+    request: (DCCDataCollectorBeginTransactionRequest*) request
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/transactions"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"POST"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+///
+/// Ends the transaction
+/// Submits final transaction state to Knetik.io
+///  @param _id Unique ID of the transaction being finalized 
+///
+///  @param customerId customerId 
+///
+///  @param request Transaction finalization information (optional)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) endTransactionUsingPUTWithId: (NSString*) _id
+    customerId: (NSString*) customerId
+    request: (DCCDataCollectorEndTransactionRequest*) request
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter '_id' is set
+    if (_id == nil) {
+        NSParameterAssert(_id);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"_id"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/transactions/{id}/end"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+    if (_id != nil) {
+        pathParams[@"id"] = _id;
+    }
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"PUT"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+///
+/// Creates and finalizes a collection of transaction information
+/// This operation basically encapsulates beginTransaction and endTransaction semantics into a single step and is used to update user balance information in Knetik.io
+///  @param customerId customerId 
+///
+///  @param request Collection state information (optional)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) updateCollectionUsingPOSTWithCustomerId: (NSString*) customerId
+    request: (DCCDataCollectorUpdateCollectionRequest*) request
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/collections"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"POST"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+///
+/// Updates the progress for the given transaction
+/// Use the event properties to update the state of the transaction
+///  @param _id Unique ID of the transaction being updated 
+///
+///  @param customerId customerId 
+///
+///  @param request Transaction progress information (optional)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) updateTransactionUsingPUTWithId: (NSString*) _id
+    customerId: (NSString*) customerId
+    request: (DCCDataCollectorUpdateTransactionRequest*) request
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter '_id' is set
+    if (_id == nil) {
+        NSParameterAssert(_id);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"_id"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCTransactionsApiErrorDomain code:kDCCTransactionsApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/transactions/{id}"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+    if (_id != nil) {
+        pathParams[@"id"] = _id;
+    }
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"PUT"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+
+
+@end
---
+++ SwaggerClient/Model/DCCDataCollectorBatchRequest.m
@@ -0,0 +1,34 @@
+#import "DCCDataCollectorBatchRequest.h"
+
+@implementation DCCDataCollectorBatchRequest
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"batch": @"batch" }];
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"batch"];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
