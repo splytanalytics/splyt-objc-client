38621d2355d2323fa619a1038b372f7eb3d63f3b
stoutfiles <shawn.stout@knetik.com>
2017-04-18 16:06:58 -0400

    JSAPI Objective C API update

---
+++ DataCollectorClient/Api/DCCUsersApi.m
@@ -0,0 +1,222 @@
+#import "DCCUsersApi.h"
+#import "DCCQueryParamCollection.h"
+#import "DCCApiClient.h"
+#import "DCCDataCollectorNewUserRequest.h"
+#import "DCCDataCollectorUpdateUserStateRequest.h"
+
+
+@interface DCCUsersApi ()
+
+@property (nonatomic, strong, readwrite) NSMutableDictionary *mutableDefaultHeaders;
+
+@end
+
+@implementation DCCUsersApi
+
+NSString* kDCCUsersApiErrorDomain = @"DCCUsersApiErrorDomain";
+NSInteger kDCCUsersApiMissingParamErrorCode = 234513;
+
+@synthesize apiClient = _apiClient;
+
+#pragma mark - Initialize methods
+
+- (instancetype) init {
+    return [self initWithApiClient:[DCCApiClient sharedClient]];
+}
+
+
+-(instancetype) initWithApiClient:(DCCApiClient *)apiClient {
+    self = [super init];
+    if (self) {
+        _apiClient = apiClient;
+        _mutableDefaultHeaders = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
+#pragma mark -
+
+-(NSString*) defaultHeaderForKey:(NSString*)key {
+    return self.mutableDefaultHeaders[key];
+}
+
+-(void) setDefaultHeaderValue:(NSString*) value forKey:(NSString*)key {
+    [self.mutableDefaultHeaders setValue:value forKey:key];
+}
+
+-(NSDictionary *)defaultHeaders {
+    return self.mutableDefaultHeaders;
+}
+
+#pragma mark - Api Methods
+
+///
+/// Submit a new user event
+/// Declares to the Knetik.io platform that the user is new at the given point in time. If the 'checked' parameter is provided and set to 'true', however, the current state of the user in the Knetik.io platform is examined to determine if the user was previously declared as new and, if so, the user information is not updated in the Knetik.io platform.
+///  @param customerId customerId 
+///
+///  @param request New user information (optional)
+///
+///  @param checked Flag indicating whether the user state should be checked before updating the state in the Knetik.io platform (optional, default to false)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) newUserWithCustomerId: (NSString*) customerId
+    request: (DCCDataCollectorNewUserRequest*) request
+    checked: (NSNumber*) checked
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCUsersApiErrorDomain code:kDCCUsersApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/users"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (checked != nil) {
+        queryParams[@"checked"] = checked;
+    }
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"POST"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+///
+/// Updates the entity state for the given user
+/// 
+///  @param _id ID of the user for whom state is being updated 
+///
+///  @param customerId customerId 
+///
+///  @param request Updated user state information (optional)
+///
+///  @returns void
+///
+-(NSURLSessionTask*) updateUserStateWithId: (NSString*) _id
+    customerId: (NSString*) customerId
+    request: (DCCDataCollectorUpdateUserStateRequest*) request
+    completionHandler: (void (^)(NSError* error)) handler {
+    // verify the required parameter '_id' is set
+    if (_id == nil) {
+        NSParameterAssert(_id);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"_id"] };
+            NSError* error = [NSError errorWithDomain:kDCCUsersApiErrorDomain code:kDCCUsersApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    // verify the required parameter 'customerId' is set
+    if (customerId == nil) {
+        NSParameterAssert(customerId);
+        if(handler) {
+            NSDictionary * userInfo = @{NSLocalizedDescriptionKey : [NSString stringWithFormat:NSLocalizedString(@"Missing required parameter '%@'", nil),@"customerId"] };
+            NSError* error = [NSError errorWithDomain:kDCCUsersApiErrorDomain code:kDCCUsersApiMissingParamErrorCode userInfo:userInfo];
+            handler(error);
+        }
+        return nil;
+    }
+
+    NSMutableString* resourcePath = [NSMutableString stringWithFormat:@"/v2/users/{id}"];
+
+    NSMutableDictionary *pathParams = [[NSMutableDictionary alloc] init];
+    if (_id != nil) {
+        pathParams[@"id"] = _id;
+    }
+
+    NSMutableDictionary* queryParams = [[NSMutableDictionary alloc] init];
+    if (customerId != nil) {
+        queryParams[@"customerId"] = customerId;
+    }
+    NSMutableDictionary* headerParams = [NSMutableDictionary dictionaryWithDictionary:self.apiClient.configuration.defaultHeaders];
+    [headerParams addEntriesFromDictionary:self.defaultHeaders];
+    // HTTP header `Accept`
+    NSString *acceptHeader = [self.apiClient.sanitizer selectHeaderAccept:@[@"*/*"]];
+    if(acceptHeader.length > 0) {
+        headerParams[@"Accept"] = acceptHeader;
+    }
+
+    // response content type
+    NSString *responseContentType = [[acceptHeader componentsSeparatedByString:@", "] firstObject] ?: @"";
+
+    // request content type
+    NSString *requestContentType = [self.apiClient.sanitizer selectHeaderContentType:@[@"application/json"]];
+
+    // Authentication setting
+    NSArray *authSettings = @[];
+
+    id bodyParam = nil;
+    NSMutableDictionary *formParams = [[NSMutableDictionary alloc] init];
+    NSMutableDictionary *localVarFiles = [[NSMutableDictionary alloc] init];
+    bodyParam = request;
+
+    return [self.apiClient requestWithPath: resourcePath
+                                    method: @"PUT"
+                                pathParams: pathParams
+                               queryParams: queryParams
+                                formParams: formParams
+                                     files: localVarFiles
+                                      body: bodyParam
+                              headerParams: headerParams
+                              authSettings: authSettings
+                        requestContentType: requestContentType
+                       responseContentType: responseContentType
+                              responseType: nil
+                           completionBlock: ^(id data, NSError *error) {
+                                if(handler) {
+                                    handler(error);
+                                }
+                            }];
+}
+
+
+
+@end
