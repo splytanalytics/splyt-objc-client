371ec54bcdac7ebe008f32b34fcbbd41c9e244a5
Tyler Novotny <tyler@knetik.com>
2017-07-18 08:49:04 -0400

    JSAPI Objective C API update

---
+++ DataCollectorClient/Model/DCCDataCollectorUpdateTransactionRequest.m
@@ -0,0 +1,34 @@
+#import "DCCDataCollectorUpdateTransactionRequest.h"
+
+@implementation DCCDataCollectorUpdateTransactionRequest
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:@{ @"progress": @"progress", @"transactionId": @"transaction_id", @"category": @"category" }];
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"transactionId", ];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
