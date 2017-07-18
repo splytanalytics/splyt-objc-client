22d126b63f059356a83dab039fdcd92b7c9367eb
Tyler Novotny <tyler@knetik.com>
2017-07-18 14:14:06 -0400

    JSAPI Objective C API update

---
+++ DataCollectorClient/Model/DCCDataCollectorUpdateTransactionRequest.m
@@ -0,0 +1,42 @@
+#import "DCCDataCollectorUpdateTransactionRequest.h"
+
+@implementation DCCDataCollectorUpdateTransactionRequest
+
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    // initialize property's default value, if any
+    
+  }
+  return self;
+}
+
+
+/**
+ * Maps json key to property name.
+ * This method is used by `JSONModel`.
+ */
++ (JSONKeyMapper *)keyMapper {
+  return [[JSONKeyMapper alloc] initWithModelToJSONDictionary:[self modalDictionary]];
+}
+
++ (NSDictionary *)modalDictionary {
+  NSMutableDictionary * mutableDictionary = [[NSMutableDictionary alloc] initWithDictionary:[super modalDictionary] copyItems:NO];
+    
+  [mutableDictionary addEntriesFromDictionary:@{ @"progress": @"progress", @"transactionId": @"transaction_id", @"category": @"category" }];
+    
+  return mutableDictionary;
+}
+
+/**
+ * Indicates whether the property with the given name is optional.
+ * If `propertyName` is optional, then return `YES`, otherwise return `NO`.
+ * This method is used by `JSONModel`.
+ */
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+
+  NSArray *optionalProperties = @[@"transactionId", ];
+  return [optionalProperties containsObject:propertyName];
+}
+
+@end
---
+++ docs/DCCBatchRequestResult.md
@@ -0,0 +1,12 @@
+# DCCBatchRequestResult
+
+## Properties
+Name | Type | Description | Notes
+------------ | ------------- | ------------- | -------------
+**code** | **NSNumber*** | HTTP response code for the processed request | [optional] 
+**errors** | **NSArray&lt;NSString*&gt;*** | Error messages if the request could not be processed | [optional] 
+**request** | [**DCCDataCollectorBaseRequest***](DCCDataCollectorBaseRequest.md) | The request. Will be null if request was successfully processed | [optional] 
+
+[[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
+
+
